8d5d4db3e928e7803687b1fc906c9a9778b87045
Clifford Morin <cemorin21@gmail.com>
2021-12-08 02:54:18 -0500

    random color and number generator using state

--- State/src/App.js
+++ State/src/App.js
@@ -1,9 +1,11 @@
 import Game from './Game';
+import Random from './Random';
 
 function App() {
 	return (
 		<div className="App">
-			<Game />
+			<Game name="Clifford" />
+			<Random maxNum="999" color="#0000FF" />
 		</div>
 	);
 }
---
+++ State/src/Random.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Random extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			num: 0
+		};
+		this.makeTimer();
+	}
+	makeTimer() {
+		setInterval(() => {
+			let rand = Math.floor(Math.random() * this.props.maxNum);
+			let randColor = Math.floor(Math.random() * 16777215).toString(16);
+			this.setState({ num: rand, color: '#' + randColor });
+		}, 1000);
+	}
+	render() {
+		return <h1 style={{ color: this.state.color }}>{this.state.num}</h1>;
+	}
+}
+
+export default Random;
