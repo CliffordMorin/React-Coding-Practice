593b716409b12fb834332e36bcc25983a4db3191
Clifford Morin <cemorin21@gmail.com>
2022-01-10 22:56:45 -0500

    added dispatch and redux works '

---
+++ redux-practice/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch()
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1> Counter : {counter} </h1>
+        <button onClick={()=> dispatch({type: 'INCREMENT'})}> Increment </button>
+        <button onClick={()=> dispatch({type: 'DECREMENT'})}> Decrement </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
