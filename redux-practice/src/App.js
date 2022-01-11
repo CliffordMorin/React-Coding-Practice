94100c63ddcb383cf2f70bad5ac2a0f0672c4138
Clifford Morin <cemorin21@gmail.com>
2022-01-11 01:49:04 -0500

    added randomColor gen in redux

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -5,16 +5,30 @@ import Login from "./Login";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const randomColor = useSelector((state) => state.colorGen);
   const dispatch = useDispatch();
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1> Counter : {counter} </h1>
-        <button onClick={() => dispatch({ type: "INCREMENT" })}>
+        <h1>
+          {" "}
+          Counter : <span style={{ color: randomColor }}>{counter}</span>{" "}
+        </h1>
+        <button
+          onClick={() => {
+            dispatch({ type: "INCREMENT" });
+            dispatch({ type: "RANDOM_COLOR" });
+          }}
+        >
           {" "}
           Increment{" "}
         </button>
-        <button onClick={() => dispatch({ type: "DECREMENT" })}>
+        <button
+          onClick={() => {
+            dispatch({ type: "DECREMENT" });
+            dispatch({ type: "RANDOM_COLOR" });
+          }}
+        >
           {" "}
           Decrement{" "}
         </button>
